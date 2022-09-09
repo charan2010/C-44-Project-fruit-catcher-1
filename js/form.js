785741adae128132bb54806e6f389b41a8afc3a4
charan2010 <84585840+charan2010@users.noreply.github.com>
2022-09-09 14:16:39 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 class Form{
     constructor(){
-       this.input = createInput("Name");
+       this.input = createInput("").attr("placeholder", "Name");
        this.button = createButton('Play');
        this.greeting = createElement('h2');
        this.title = createElement('h2');
